8ec86424803f656874f334527f7fa6ef109bd71e
ydkulks <ydkulks2@gmail.com>
2023-10-01 14:40:10 +0530

    Added link and copyright symbol
    Link - Added link your github profile in the footer
    Copyright - Added copyright symbol instead of @

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="px-4">
       <div className="w-100% border-t-2 h-12 flex justify-between px-3 items-center">
-        <p>Made by Tensei</p>
-        <p>copyright@2023</p>
+        <p>Made by <a href="https://github.com/DevTenz">Tensei</a></p>
+        <p>copyright©2023</p>
       </div>
     </footer>
   );
